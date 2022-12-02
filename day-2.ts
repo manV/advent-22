7749aeac7bd860850d7d5fb68cd7a723d736f168
Manan Vaghasiya <manan@ironmanan.com>
2022-12-02 12:09:08 +0000

    day2-p2

--- day-2.ts
+++ day-2.ts
@@ -1,36 +1,42 @@
 const data = await Deno.readTextFile("./day-2-input.txt");
 
 type OpponentsPlay = "A" | "B" | "C";
-type YourPlay = "X" | "Y" | "Z";
+type YourDesiredResult = "X" | "Y" | "Z";
 
-const yourHandScores: {
-  [x in YourPlay]: number;
+const winScore: {
+  [x in YourDesiredResult]: {
+    [x in OpponentsPlay]: number;
+  };
 } = {
-  X: 1,
-  Y: 2,
-  Z: 3,
+  X: {
+    A: 3,
+    B: 1,
+    C: 2,
+  },
+  Y: {
+    A: 1,
+    B: 2,
+    C: 3,
+  },
+  Z: {
+    A: 2,
+    B: 3,
+    C: 1,
+  },
 };
 
-const drawHand: {
-  [x in OpponentsPlay]: YourPlay;
+const resultScore: {
+  [x in YourDesiredResult]: number;
 } = {
-  A: "X",
-  B: "Y",
-  C: "Z",
-};
-
-const winHand: {
-  [x in OpponentsPlay]: YourPlay;
-} = {
-  A: "Y",
-  B: "Z",
-  C: "X",
+  X: 0,
+  Y: 3,
+  Z: 6,
 };
 
 const result = data
   .split("\n")
   .map((play) => {
-    return play.split(" ") as [OpponentsPlay, YourPlay];
+    return play.split(" ") as [OpponentsPlay, YourDesiredResult];
   })
   .reduce((prev, curr) => {
     return prev + getScore(curr[0], curr[1]);
@@ -38,11 +44,11 @@ const result = data
 
 console.log("your total score", result);
 
-function getScore(opponentsPlay: OpponentsPlay, yourPlay: YourPlay) {
-  if (winHand[opponentsPlay] === yourPlay) {
-    return 6 + yourHandScores[yourPlay];
-  } else if (drawHand[opponentsPlay] === yourPlay) {
-    return 3 + yourHandScores[yourPlay];
-  }
-  return yourHandScores[yourPlay];
+function getScore(
+  opponentsPlay: OpponentsPlay,
+  yourDesiredResult: YourDesiredResult
+) {
+  return (
+    resultScore[yourDesiredResult] + winScore[yourDesiredResult][opponentsPlay]
+  );
 }
