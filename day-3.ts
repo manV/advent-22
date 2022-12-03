16fcf08894f0fd065c1dced292394bccfe420df9
Manan Vaghasiya <manan@ironmanan.com>
2022-12-03 16:10:48 +0000

    day3-p2

--- day-3.ts
+++ day-3.ts
@@ -5,14 +5,21 @@ for (let i = 1; i <= 52; i++) {
   priorityArray.push(i);
 }
 
-const res = data
-  .split("\n")
-  .map((rucksackItem) => {
-    const compLen = rucksackItem.length / 2;
-    return [rucksackItem.slice(0, compLen), rucksackItem.slice(compLen)];
-  })
-  .map(([comp1, comp2]) => {
-    const commonChar = findCommonCharactor(comp1, comp2);
+const groups = [];
+let temp = [];
+const allRucksacks = data.split("\n");
+
+for (let i = 0; i < allRucksacks.length; i++) {
+  temp.push(allRucksacks[i]);
+  if ((i + 1) % 3 === 0) {
+    groups.push(temp);
+    temp = [];
+  }
+}
+
+const res = groups
+  .map((group) => {
+    const commonChar = findCommonCharactor(group[0], group[1], group[2]);
     if (!commonChar) throw new Error("common char not found");
     return findPriority(commonChar);
   })
@@ -22,10 +29,21 @@ const res = data
 
 console.log(res);
 
-function findCommonCharactor(str1: string, str2: string) {
-  for (const c1 of str1) {
-    if (str2.includes(c1)) return c1;
+function findCommonCharactor(
+  ruck1: string,
+  ruck2: string,
+  ruck3: string
+): string | null {
+  const commonChars: string[] = [];
+  for (const c1 of ruck1) {
+    if (ruck2.includes(c1) && !commonChars.includes(c1)) {
+      commonChars.push(c1);
+    }
   }
+  for (const c3 of ruck3) {
+    if (commonChars.includes(c3)) return c3;
+  }
+
   return null;
 }
 
